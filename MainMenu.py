d2274b7232a0d8b8429eb20f4d503d7d3e7e2c8c
RGingrich8 <richard.gingrich@ucalgary.ca>
2019-06-05 00:37:26 -0600

    Add files via upload

--- MainMenu.py
+++ MainMenu.py
@@ -1,15 +1,26 @@
 class MainMenu:  
     def __init__(self, day_number): #Constructor
         self.day_number = day_number
+        self.options = ["B - Buy Supplies", "S - Set Recipe and Prices", "V - View Career", "Q - Quit Game", "A - Advance to Day"]
+        self.commands = ["B", "S", "V", "A", "Q"]
 
-    def menu(self):
+
+    def menu(self): #Run the menu and its options
         print("It is day " + str(self.day_number))
-        self.day_number += 1
-    
+        self.list_options()
 
-    def get_day(self):
-    	return self.day_number
+        user_in = str(input("Which action would you like to take? ")).upper()
+        while user_in not in self.commands: #While not a valid action, repeat the question
+        	user_in = str(input("Invalid action. Enter one of BSVA: ")).upper()
+        return user_in #Return the user's choice
 
-    def day(self):
-        pass
     
+    def list_options(self): #Print out all of the options for the user
+    	for elem in self.options:
+    		print(elem)
+
+    
+    def inc_day(self): #Run the day and all of its actions
+    	self.day_number += 1
+
+ 
--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from MainMenu import *
+from Shop import *
 
 #Variables
 run_main = MainMenu(1) #Create main menu object
+the_shop = Shop(1.00, 0.75, 0.30, 0.10) #Initialize shop costs
 
 
 def intro(): #Get player name and stand name
     p_n = str(input("What is your name? "))
     s_n = str(input("What is the name of your stand? "))
     return p_n, s_n
-    #tutorial_question = str(input("Do you need a tutorial? ")).upper()
-    #if tutorial_question == "Y":
-    #    return True
-    #if tutorial_question == "N":
-    #    return False
-
-
-
 
 
 def main(): #Run everything in order
@@ -23,10 +17,25 @@ def main(): #Run everything in order
     game_running = True #Game runs
     
     while game_running:
-        print("You are currently managing " + stand_name)
-        run_main.menu()
+        print("\nYou are currently managing " + stand_name)
+        
+        action_taken = run_main.menu()
+        if (action_taken == "B"): #Buy
+            print("\n\n>>>Items to be purchased<<<")
+            the_shop.print_prices()
+            the_shop.buy()
+        elif (action_taken == "S"): #Set recipe
+            pass
+        elif (action_taken == "V"): #Career
+            pass
+        elif (action_taken == "A"): #Advance day
+            pass
+            run_main.inc_day() #End of day, add one
+        elif (action_taken == "Q"):
+            game_running = False
 
-        if (run_main.get_day() == 31): #Ends at 30
+
+        if (run_main.day_number == 31): #Ends at 30
             #Outro here
             game_running = False
     
