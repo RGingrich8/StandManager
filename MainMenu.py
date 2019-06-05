d89541f2cfe5e1e0adb4cd8079621ca2629259e5
RGingrich8 <richard.gingrich@ucalgary.ca>
2019-06-04 21:53:52 -0600

    First

---
+++ MainMenu.py
@@ -0,0 +1,10 @@
+class MainMenu:  
+    def __init__(self, day_number): #Constructor
+        self.day_number = day_number
+
+    def menu(self):
+        print("It is day " + str(self.day_number))
+    
+    def day():
+        pass
+    
---
+++ main.py
@@ -0,0 +1,31 @@
+from MainMenu import *
+
+#Variables
+player_name = ""
+stand_name = ""
+run_main = MainMenu(1) #Create main menu object
+
+
+def intro(): #Get player name and stand name
+    player_name = str(input("What is your name? "))
+    stand_name = str(input("What is the name of your stand? "))
+    #tutorial_question = str(input("Do you need a tutorial? ")).upper()
+    #if tutorial_question == "Y":
+    #    return True
+    #if tutorial_question == "N":
+    #    return False
+
+
+
+
+
+def main(): #Run everything in order
+    intro()
+    game_running = True
+    
+    while game_running:
+        print("You are currently managing " + stand_name)
+        run_main.menu()
+    
+    
+main() #Run
